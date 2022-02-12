99e8301fbfe42363b0561b814e680e24ec43a132
Michael Matus <michael@matus.at>
2022-02-12 07:50:36 +0100

    Bev.UI removed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Bev.IO.NmmReader;
-using Bev.UI;
 
 namespace NmmSensors
 {
@@ -20,22 +16,16 @@ namespace NmmSensors
 
             if (!CommandLine.Parser.Default.ParseArgumentsStrict(args, options))
                 Console.WriteLine("*** ParseArgumentsStrict returned false");
-            if (options.BeQuiet == true)
-                ConsoleUI.BeSilent();
-            else
-                ConsoleUI.BeVerbatim();
-            ConsoleUI.Welcome();
 
             // get the filename(s)
             string[] fileNames = options.ListOfFileNames.ToArray();
             if (fileNames.Length == 0)
-                ConsoleUI.ErrorExit("!Missing input file", 1);
+                ErrorExit("!Missing input file", 1);
 
-            ConsoleUI.ReadingFile(fileNames[0]);
+
             NmmFileName nmmFileName = new NmmFileName(fileNames[0]);
             nmmFileName.SetScanIndex(options.ScanIndex);
             NmmEnvironmentData sensors = new NmmEnvironmentData(nmmFileName);
-            ConsoleUI.Done();
 
             //testing
             StringBuilder sb = new StringBuilder();
@@ -53,5 +43,8 @@ namespace NmmSensors
 
             Console.WriteLine(sb.ToString());
         }
+
+        static void ErrorExit(string message, int code)
+        { }
     }
 }
